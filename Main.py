1bcaeaccc7a6381bda1d54a9b6087810f0feec65
vow0w <36414599+vow0w@users.noreply.github.com>
2020-03-08 22:48:29 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,51 +8,51 @@ import datetime
 import change_site
 from threading import Timer
 import os
-try:
-    def write_msg(user_id, message):
-        vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2048)})
 
-    token = os.environ['VK_TOKEN']
-    vk = vk_api.VkApi(token=token)
-    api = vk.get_api()
-    longpoll = VkLongPoll(vk)
-    commander = Commander()
-    print("Бот запущен")
+def start():
+    try:
+        x = datetime.datetime.today()
+        y = x.replace(day=x.day, hour=15, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
+        delta_t = y - x
 
-    #Рассылка
-    x = datetime.datetime.today()
-    y = x.replace(day=x.day, hour=15, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
-    delta_t = y - x
-    secs = delta_t.total_seconds()
-    def send_message():
-        i = 0
-        while i < 4:
-            users = [186003041, 288925718, 525452357, 187419279]
-            change = "Замены на завтра: \n" + change_site.start()
-            api.messages.send(user_id=users[i], message=change, random_id=get_random_id())
-            i = i + 1
-    t = Timer(secs, send_message)
-    t.start()
+        secs = delta_t.total_seconds()
 
-    for event in longpoll.listen():
+        def write_msg(user_id, message):
+            vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2048)})
 
-        if event.attachments.items():
-            continue
+        token = os.environ['VK_TOKEN']
+        vk = vk_api.VkApi(token=token)
+        longpoll = VkLongPoll(vk)
+        commander = Commander()
+        api = vk.get_api()
+        print("Бот запущен")
 
-        if event.type == VkEventType.MESSAGE_NEW:
+        def send_message():
+            i = 0
+            while i < 4:
+                users = [186003041, 288925718, 525452357, 187419279]
+                change = "Замены на завтра: \n" + change_site.start()
+                api.messages.send(user_id=users[i], message=change, random_id=get_random_id())
+                i = i + 1
 
-            if event.to_me:
+        t = Timer(secs, send_message)
+        t.start()
 
-                print(f'Новое сообщение от: {event.user_id}', end='')
+        for event in longpoll.listen():
+            if event.attachments.items():
+                continue
+            if event.type == VkEventType.MESSAGE_NEW:
+                if event.to_me:
+                    print(f'Новое сообщение от: {event.user_id}', end='')
+                    bot = VkBot(event.user_id)
+                    if event.text[0] == "":
+                        write_msg(event.user_id, commander.do(event.text[1::]))
+                    else:
+                        write_msg(event.user_id, bot.new_message(event.text))
+                    print('Текст: ', event.text)
+                    print("-------------------")
+    except:
+        print("Перезапуск")
+        start()
 
-                bot = VkBot(event.user_id)
-
-                if event.text[0] == "":
-                    write_msg(event.user_id, commander.do(event.text[1::]))
-                else:
-                    write_msg(event.user_id, bot.new_message(event.text))
-
-                print('Текст: ', event.text)
-                print("-------------------")
-except:
-    print("А я не упал!")
+start()
